9acd759d047fed85696482650af3cad5e0d8c929
David Ladowitz + Jessie Young <apprentice+dl+jy@devbootcamp.com>
2012-08-07 16:06:17 -0700

    adding progress bars

--- app/models/goal.rb
+++ app/models/goal.rb
@@ -1,5 +1,5 @@
 class Goal < ActiveRecord::Base
-  attr_accessible :description, :due_date, :user_id, :objectives_attributes
+  attr_accessible :description, :due_date, :user_id, :objectives_attributes, :created_at
 
   validates_presence_of :user_id, :description, :due_date
 
@@ -8,5 +8,14 @@ class Goal < ActiveRecord::Base
   has_many :steps, :through => :objectives
 
   accepts_nested_attributes_for :objectives
+
+
+
+  def percentage_complete
+    total_days = (self.due_date.to_date - self.created_at.to_date).to_i
+    daily_rate = self.objectives.first.frequency.to_f / 7
+    expected_steps = total_days * daily_rate
+    ((self.objectives.first.steps.count.to_f / expected_steps) * 100 ).floor
+  end
 
 end
