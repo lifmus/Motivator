87d704d2b598752635557223d382eef223c1e39b
Andrew Lin + Jeremy Reyes <apprentice+al+jr@devbootcamp.com>
2012-08-07 14:18:14 -0700

    fabricator changes

--- spec/fabricators/goal_fabricator.rb
+++ spec/fabricators/goal_fabricator.rb
@@ -1,5 +1,9 @@
 Fabricator(:goal) do
   description { Faker::Lorem.sentence(1) }
   due_date { Time.now + 6.months }
-  user_id { 1 } # TODO change when we have the user model
+  user_id { Fabricate(:user) } # TODO change when we have the user model
+end
+
+Fabricator(:goal_with_objective, :from => :goal) do
+  objectives(:count => 1) { Fabricate(:objective) }
 end
