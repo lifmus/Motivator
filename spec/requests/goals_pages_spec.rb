31850c310ca36585d92663005681fb7e6de66509
Andrew Lin + Jeremy Reyes <apprentice+al+jr@devbootcamp.com>
2012-08-06 18:17:47 -0700

    added view tests for goal#show

---
+++ spec/requests/goals_pages_spec.rb
@@ -0,0 +1,46 @@
+#TODO move to views folder when we figure out why this file doesn't work from there
+require 'spec_helper'
+
+describe "Goals pages" do
+  subject { page }
+  let(:user) { Fabricate(:user) }
+  let(:goal) { Fabricate(:goal) }
+
+  describe "goals#show" do
+
+    describe "when logged out" do
+      before { visit goal_path(goal) }
+      it { should have_content('Sign in') }
+    end
+
+    describe "when logged in" do
+          before do
+            visit new_user_session_path
+            fill_in "Email", with: user.email
+            fill_in "Password", with: user.password
+            click_button "Sign in"
+            visit goal_path(goal)
+          end
+          it { should have_content(goal.description) }
+          it { should have_content('When did you') }
+          it { should have_content(goal.due_date) }
+
+          # describe "'New Project' creates a new project in the database" do
+          #         before do
+          #           fill_in "Name", with: "Googly Moogly"
+          #           fill_in "Description", with: "F'in things up!"
+          #         end
+          #
+          #         it "should create the project" do
+          #           expect { click_button 'Create Project'}.to change(Project, :count).by(1)
+          #         end
+          #
+          #         describe "displays the project in the list of projects" do
+          #           before { click_button 'Create Project' }
+          #           it { should have_content("Googly Moogly") }
+          #           it { should have_content("F'in things up!") }
+          #         end
+                end
+
+  end
+end
