30e832ebb0a61dafc07313635b21f09d3ab47a2a
David Ladowitz + Andrew Lin <apprentice+dl+al@devbootcamp.com>
2012-08-08 17:51:40 -0700

    nothing changed, ready to pull

--- app/controllers/creditcards_controller.rb
+++ app/controllers/creditcards_controller.rb
@@ -5,20 +5,20 @@ class CreditcardsController < ApplicationController
   end
 
   def create
-    if current_user.name
-      @name = current_user.name
-    else
-      @name = "not given"
-    end
-      customer = Stripe::Customer.create(
-      :card => params[:stripeToken],
-      :email => current_user.email,
-      :description => @name
-    )
+      if current_user.name
+        @name = current_user.name
+      else
+        @name = "not given"
+      end
+        customer = Stripe::Customer.create(
+        :card => params[:stripeToken],
+        :email => current_user.email,
+        :description => @name
+      )
 
-    current_user.stripe_customer_id = customer.id
-    current_user.save
-    redirect_to(root_path)
+      current_user.stripe_customer_id = customer.id
+      current_user.save
+      redirect_to(root_path)
   end
 
   def edit
