6a1f675cfd76ccd283a0dfbdc62caec59fa27da7
David Ladowitz + Jeremy Reyes <apprentice+dl+jr@devbootcamp.com>
2012-08-13 16:29:16 -0700

    fixed migrations

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120813222936) do
+ActiveRecord::Schema.define(:version => 20120813232841) do
 
   create_table "charges", :force => true do |t|
     t.integer  "goal_id"
