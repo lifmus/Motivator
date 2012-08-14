563c2e01eba0fb189bb7824e58d23c6abb3cc78f
David Ladowitz + Jeremy Reyes <apprentice+dl+jr@devbootcamp.com>
2012-08-14 16:11:38 -0700

    devise views, gravatar/facebook profile pics

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,13 +2,14 @@ module ApplicationHelper
   def gravatar_for(user)
     gravatar_id = Digest::MD5::hexdigest(user.email.downcase)
     gravatar_url = "https://secure.gravatar.com/avatar/#{gravatar_id}"
-    image_tag(gravatar_url, alt: user.name, class: "gravatar")
+    image_tag(gravatar_url, alt: user.name, class: "gravatar", size: "50x50")
   end
 
   def profile_pic_for(user)
     if user.image != nil
       image_tag(user.image, size: "50x50")
     else
+      gravatar_for(user)
     end
   end
 
