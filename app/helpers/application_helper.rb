ebdf162747d92a412d6b84148a3da0f168206c28
David Ladowitz + Jeremy Reyes <apprentice+dl+jr@devbootcamp.com>
2012-08-14 12:25:29 -0700

    add gravatar and facebook profile pics

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,15 @@
 module ApplicationHelper
+  def gravatar_for(user)
+    gravatar_id = Digest::MD5::hexdigest(user.email.downcase)
+    gravatar_url = "https://secure.gravatar.com/avatar/#{gravatar_id}"
+    image_tag(gravatar_url, alt: user.name, class: "gravatar")
+  end
+
+  def profile_pic_for(user)
+    if user.image != nil
+      image_tag(user.image, size: "50x50")
+    else
+    end
+  end
+
 end
